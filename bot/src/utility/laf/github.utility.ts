8576986e9a9afd155c227ac9baae6442db16abb4
FOCKUSTY <wwddwwwd@mail.ru>
2025-02-05 21:01:28 +0500

    -up types

--- bot/src/utility/laf/github.utility.ts
+++ bot/src/utility/laf/github.utility.ts
@@ -1,55 +1,11 @@
 import { Env } from "@voidy/develop/dist";
 import Formatter from "f-formatter";
 
-interface Repo {
-    "id": number,
-    "node_id": string,
-    "name": string,
-    "full_name": string,
-    "owner": {
-        "login": string,
-        "id": number,
-        "node_id": string,
-        "avatar_url": string,
-        "gravatar_id": string,
-        "url": string,
-        "html_url": string,
-        "type": string,
-        "site_admin": boolean
-    },
-    "private": boolean,
-    "html_url": string,
-    "description": string,
-    "fork": boolean,
-    "language": string,
-    "forks_count": number,
-    "stargazers_count": number,
-    "watchers_count": number,
-    "size": number,
-    "default_branch": string,
-    "open_issues_count": number,
-    "is_template": boolean,
-    "topics": string[],
-    "has_issues": boolean,
-    "has_projects": boolean,
-    "has_wiki": boolean,
-    "has_pages": boolean,
-    "has_downloads": boolean,
-    "archived": boolean,
-    "disabled": boolean,
-    "visibility": string,
-    "pushed_at": Date,
-    "created_at": Date,
-    "updated_at": Date,
-    "allow_rebase_merge": boolean,
-    "forks": number,
-    "open_issues": number,
-    "watchers": number
-};
+import { Repo, GitHubApi as Service } from "@voidy/types/dist/utils/github.type";
 
 const formatter = new Formatter();
 
-class GitHubApi {
+class GitHubApi extends Service {
     public async getRepositories(ignoredRepo: string[] = [".github"]): Promise<Repo[]> {
         const data: Repo[] = await (await fetch(Env.get("GITHUB_REPOS_URL"), {
             method: "GET",
