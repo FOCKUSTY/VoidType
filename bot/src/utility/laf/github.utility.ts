66e599829329a533f553bae6cd05197da2b1ff6b
FOCKUSTY <wwddwwwd@mail.ru>
2025-02-05 20:58:30 +0500

    -up github.utility.ys

--- bot/src/utility/laf/github.utility.ts
+++ bot/src/utility/laf/github.utility.ts
@@ -1,6 +1,7 @@
 import { Env } from "@voidy/develop/dist";
+import Formatter from "f-formatter";
 
-type Repo = {
+interface Repo {
     "id": number,
     "node_id": string,
     "name": string,
@@ -13,15 +14,6 @@ type Repo = {
         "gravatar_id": string,
         "url": string,
         "html_url": string,
-        "followers_url": string,
-        "following_url": string,
-        "gists_url": string,
-        "starred_url": string,
-        "subscriptions_url": string,
-        "organizations_url": string,
-        "repos_url": string,
-        "events_url": string,
-        "received_events_url": string,
         "type": string,
         "site_admin": boolean
     },
@@ -29,12 +21,37 @@ type Repo = {
     "html_url": string,
     "description": string,
     "fork": boolean,
-    "url": string
-}[];
+    "language": string,
+    "forks_count": number,
+    "stargazers_count": number,
+    "watchers_count": number,
+    "size": number,
+    "default_branch": string,
+    "open_issues_count": number,
+    "is_template": boolean,
+    "topics": string[],
+    "has_issues": boolean,
+    "has_projects": boolean,
+    "has_wiki": boolean,
+    "has_pages": boolean,
+    "has_downloads": boolean,
+    "archived": boolean,
+    "disabled": boolean,
+    "visibility": string,
+    "pushed_at": Date,
+    "created_at": Date,
+    "updated_at": Date,
+    "allow_rebase_merge": boolean,
+    "forks": number,
+    "open_issues": number,
+    "watchers": number
+};
+
+const formatter = new Formatter();
 
 class GitHubApi {
-    public async getRepositories(ignoredRepo: string[] = [".github"]): Promise<Repo> {
-        const data: Repo = await (await fetch(Env.get("GITHUB_REPOS_URL"), {
+    public async getRepositories(ignoredRepo: string[] = [".github"]): Promise<Repo[]> {
+        const data: Repo[] = await (await fetch(Env.get("GITHUB_REPOS_URL"), {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -42,7 +59,21 @@ class GitHubApi {
             }
         })).json();
 
-        return data.filter(r => !ignoredRepo.includes(r.name));
+        return data.filter((r: Repo) => !ignoredRepo.includes(r.name));
+    }
+
+    /**
+     * @param getRepository - A some repository
+     * @param dateOffset - A date offset in seconds
+     */
+    public repositoryCommited(repository: Repo, dateOffset: number): boolean {
+        const now = formatter.date.Timestamp(new Date(), "miliseconds");
+        const last_commit = formatter.date.Timestamp(repository.pushed_at, "miliseconds");
+
+        if (last_commit - now < dateOffset)
+            return false;
+
+        return true;
     }
 }
 
